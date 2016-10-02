d8a9b2f7680fc51a02782e27deb1ba7a77a9bb0e
Brandon Unbedacht <unbedachtb@gmail.com>
2016-10-02 00:01:22 -0600

    did some restructuring to the UI

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,1 +1,14 @@
-angular.module("app", []);
+angular.module("app", ["ui.router"]).config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'views/homeview.html',
+            controller: 'homeCtrl'
+        })
+        // .state('chart', {
+        //     url: '/chart',
+        //     templateUrl: '../views/chartview.html'
+        // })
+    $urlRouterProvider
+        .otherwise('/');
+});
